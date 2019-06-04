9def0ebabfaa1927f31f76d378252e8eee4b17cd
huwenxiang <282543820@qq.com>
2019-06-04 19:57:19 +0800

    修改了一个bug，把num_label错当成了batch_size

--- demo.py
+++ demo.py
@@ -5,14 +5,19 @@ import create_input
 import tokenization
 import numpy as np
 
+
+
+
 # 这里是下载下来的bert配置文件
+
 bert_config = modeling.BertConfig.from_json_file("chinese_L-12_H-768_A-12/bert_config.json")
 vocab_file="chinese_L-12_H-768_A-12/vocab.txt"
-num_labels=20
+batch_size=20
+num_labels=2# 类别数量，我的例子是个二分类
 is_training=True
-max_seq_length=80
+max_seq_length=128
 iter_num=1000
-lr=0.01
+lr=0.00005
 if max_seq_length > bert_config.max_position_embeddings: # 模型有个最大的输入长度 512
     raise ValueError("超出模型最大长度")
 
@@ -26,7 +31,7 @@ texts=[]
 labels=[]
 for line in data:
     line=line.split("\t")
-    if len(line) ==2:
+    if len(line) ==2 and int(line[1])<2:# 这里演示一个二分类问题，但训练样本并没有认真处理过，所以去掉label大于1的。
         texts.append(line[0])
         labels.append(line[1])
 
@@ -45,17 +50,17 @@ for t in texts:
 input_idsList=np.asarray(input_idsList,dtype=np.int32)
 input_masksList=np.asarray(input_masksList,dtype=np.int32)
 segment_idsList=np.asarray(segment_idsList,dtype=np.int32)
-labels=np.asarray(labels,dtype=np.int32).reshape([-1,1])
+labels=np.asarray(labels,dtype=np.int32)
 #  创建bert的输入
-input_ids=tf.placeholder (shape=[num_labels,max_seq_length],dtype=tf.int32,name="input_ids")
-input_mask=tf.placeholder (shape=[num_labels,max_seq_length],dtype=tf.int32,name="input_mask")
-segment_ids=tf.placeholder (shape=[num_labels,max_seq_length],dtype=tf.int32,name="segment_ids")
+input_ids=tf.placeholder (shape=[batch_size,max_seq_length],dtype=tf.int32,name="input_ids")
+input_mask=tf.placeholder (shape=[batch_size,max_seq_length],dtype=tf.int32,name="input_mask")
+segment_ids=tf.placeholder (shape=[batch_size,max_seq_length],dtype=tf.int32,name="segment_ids")
 ###
-input_labels=tf.placeholder (shape=[num_labels,1],dtype=tf.int32,name="input_ids")
+input_labels=tf.placeholder (shape=batch_size,dtype=tf.int32,name="input_ids")
 # 创建bert模型
 model = modeling.BertModel(
     config=bert_config,
-    is_training=True,
+    is_training=is_training,
     input_ids=input_ids,
     input_mask=input_mask,
     token_type_ids=segment_ids,
@@ -66,11 +71,19 @@ output_layer = model.get_sequence_output()# 这个获取每个token的output 输
 output_layer = model.get_pooled_output() # 这个获取句子的output
 hidden_size = output_layer.shape[-1].value #获取输出的维度
 
+
+# 后面就简单了，就是一个全连接，效果和下面的是一样的
+# logits = tf.layers.dense(output_layer, 2)
+# loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=input_labels, name="soft_loss")
+# loss = tf.reduce_mean(loss, name="loss")
+# predict = tf.argmax(tf.nn.softmax(logits), axis=1, name="predictions")
+# acc = tf.reduce_mean(tf.cast(tf.equal(input_labels, tf.cast(predict, dtype=tf.int32)), "float"), name="accuracy")
+
+
 # 后面就简单了，就是一个全连接
 output_weights = tf.get_variable(
     "output_weights", [num_labels, hidden_size],
     initializer=tf.truncated_normal_initializer(stddev=0.02))
-
 output_bias = tf.get_variable(
     "output_bias", [num_labels], initializer=tf.zeros_initializer())
 with tf.variable_scope("loss"):
@@ -79,16 +92,12 @@ with tf.variable_scope("loss"):
         output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
     logits = tf.matmul(output_layer, output_weights, transpose_b=True)
     logits = tf.nn.bias_add(logits, output_bias)
-
-    # probabilities = tf.nn.softmax(logits, axis=-1)
     log_probs = tf.nn.log_softmax(logits, axis=-1)
     one_hot_labels = tf.one_hot(input_labels, depth=num_labels, dtype=tf.float32)
     per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
     loss = tf.reduce_mean(per_example_loss)
-
-    predictions = tf.reshape(tf.argmax(logits, axis=-1, output_type=tf.int32),shape=[-1,1],name="predictions")
-    acc = tf.reduce_mean(tf.cast(tf.equal(input_labels, predictions), "float"),
-                         name="accuracy")
+    predict = tf.argmax(tf.nn.softmax(logits), axis=1, name="predictions")
+    acc = tf.reduce_mean(tf.cast(tf.equal(input_labels, tf.cast(predict, dtype=tf.int32)), "float"), name="accuracy")
 
 train_op = tf.train.AdamOptimizer(lr).minimize(loss)
 
@@ -115,7 +124,7 @@ for var in tvars:
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(iter_num):
-        shuffIndex = np.random.permutation(np.arange(num_labels))
+        shuffIndex = np.random.permutation(np.arange(len(texts)))[:batch_size]
         batch_labels = labels[shuffIndex]
         batch_input_idsList=input_idsList[shuffIndex]
         batch_input_masksList=input_masksList[shuffIndex]
